abbdba3b2565c92ab5fb8b08816c02fbd388ec0b
Tarsisio Xavier <tarsisiox@gmail.com>
2020-01-14 20:53:08 +0000

    Finalizando Pacote de Endereços

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Agenciafmd\Addresses\Tests\Unit;
+
+use Orchestra\Testbench\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function basic_test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
